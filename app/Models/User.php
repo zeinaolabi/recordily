da51e562c0b278920e46697d694145e23c1c6b95
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-24 19:41:59 +0300

    make create user function in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,10 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
@@ -38,8 +41,17 @@ class User extends Authenticatable implements JWTSubject
         return [];
     }
 
-    public function getName(): string
+    public static function createUser($email, $password, $type): JsonResponse
     {
-        return $this->name . $this->lastP;
+        //Create salt and hash the password
+        $salt = Str::random(5);
+        $hashed = Hash::make($password . $salt);
+
+        return User::create([
+            'email' => $email,
+            'password' => $hashed,
+            'salt' => $salt,
+            'user_type_id' => $type
+        ]);
     }
 }
