9980ed24bfdc01b56f8aac6def1ce50fc7628f39
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-26 11:56:03 +0300

    add moshi converter to retrofit

--- app/src/main/java/com/example/recordily_client/apis/RetrofitInstance.kt
+++ app/src/main/java/com/example/recordily_client/apis/RetrofitInstance.kt
@@ -1,16 +1,23 @@
 package com.example.recordily_client.apis
 
+import com.example.recordily_client.services.UserService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitInstance {
+    val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
 
     val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("http://127.0.0.1/api/")
-        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("http://192.168.1.102/api/")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
 
-    val login: UserAPI = retrofit.create(UserAPI::class.java)
+    val userAPI: UserAPI by lazy {
+        retrofit.create(UserAPI::class.java)
+    }
+
+    val apiClient = UserService(userAPI);
 
 }
