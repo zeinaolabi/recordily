fbfa5dbf01a9032e5b9d6dfd9629c7575d6df5f2
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-26 12:51:05 +0300

    modify on the textfield component design

--- app/src/main/java/com/example/recordily_client/components/TextField.kt
+++ app/src/main/java/com/example/recordily_client/components/TextField.kt
@@ -7,18 +7,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 
 @Composable
-fun TextField(value: MutableState<String>, text: String) {
-    TextField(
+fun TextField(input: MutableState<String>, text: String) {
+
+    OutlinedTextField(
         modifier = Modifier
             .fillMaxWidth(.9f)
-            .clip(MaterialTheme.shapes.small)
-            .background(colorResource(R.color.gray)),
-        value = "",
-        label = { Text(text , color = colorResource(R.color.darker_gray)) },
-        onValueChange = { value.value = it }
+            .clip(MaterialTheme.shapes.small),
+        value = input.value,
+        label = { Text(text) },
+        onValueChange = { input.value = it },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            backgroundColor = Color.Transparent,
+            focusedLabelColor = MaterialTheme.colors.secondary,
+            focusedBorderColor = MaterialTheme.colors.secondary,
+            unfocusedLabelColor = colorResource(R.color.darker_gray),
+            cursorColor = MaterialTheme.colors.secondary,
+            unfocusedBorderColor = MaterialTheme.colors.secondary,
+            )
     )
 }
