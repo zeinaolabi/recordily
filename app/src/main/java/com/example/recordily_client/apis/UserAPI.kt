61324dcc6e97c967281d2a6682a7b9d329697154
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-27 22:02:16 +0300

    add register api

--- app/src/main/java/com/example/recordily_client/apis/UserAPI.kt
+++ app/src/main/java/com/example/recordily_client/apis/UserAPI.kt
@@ -2,10 +2,14 @@ package com.example.recordily_client.apis
 
 import com.example.recordily_client.responses.LoginResponse
 import com.example.recordily_client.requests.LoginRequest
+import com.example.recordily_client.responses.RegistrationResponse
 import retrofit2.Response
 import retrofit2.http.*
 
 interface UserAPI {
     @POST("login")
     suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
+
+    @POST("login")
+    suspend fun register(@Body registrationResponse: RegistrationResponse): RegistrationResponse
 }
