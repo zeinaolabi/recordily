025e98c99e7896d3476b2558947bae1b5badc92e
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-27 15:45:33 +0300

    create a main activity

---
+++ app/src/main/java/com/example/recordily_client/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.recordily_client
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.recordily_client.ui.theme.Recordily_clientTheme
+
+@ExperimentalAnimationApi
+class MainActivity : ComponentActivity() {
+    lateinit var navController: NavHostController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+            Recordily_clientTheme {
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
+        }
+    }
+}
