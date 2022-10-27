2cb00847dd0aa57615417645fef21dd95216d190
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-27 21:58:46 +0300

    create registration response data class

---
+++ app/src/main/java/com/example/recordily_client/responses/RegistrationResponse.kt
@@ -0,0 +1,10 @@
+package com.example.recordily_client.responses
+
+data class RegistrationResponse(
+    val created_at: String,
+    val email: String,
+    val id: Int,
+    val token: String,
+    val updated_at: String,
+    val user_type_id: String
+)
