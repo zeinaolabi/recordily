4e64f5654a9e10d9c5ac2d483014c4d999a9033b
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-27 22:14:17 +0300

    create registration repository

--- app/src/main/java/com/example/recordily_client/repositories/LoginRepository.kt
+++ app/src/main/java/com/example/recordily_client/repositories/LoginRepository.kt
@@ -4,8 +4,8 @@ import com.example.recordily_client.apis.RetrofitInstance
 import com.example.recordily_client.responses.LoginResponse
 import com.example.recordily_client.requests.LoginRequest
 
-class UserRepository {
+class LoginRepository {
     suspend fun login(loginRequest: LoginRequest): LoginResponse {
-        return RetrofitInstance.apiClient.login(loginRequest)
+        return RetrofitInstance.authClient.login(loginRequest)
     }
 }
