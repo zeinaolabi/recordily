3b3f369c9d066952e87a370e2b01596d83477e5c
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-27 15:46:13 +0300

    create and design the landing page

---
+++ app/src/main/java/com/example/recordily_client/CommonLandingPage.kt
@@ -0,0 +1,35 @@
+package com.example.recordily_client
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.recordily_client.components.BottomNavigationBar
+import com.example.recordily_client.components.Header
+
+@Composable
+fun CommonLandingPage(navController: NavController){
+
+    Scaffold(
+        topBar = { Header() },
+        bottomBar = { BottomNavigationBar(navController) }
+    ) {
+
+    }
+
+//    Box(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(MaterialTheme.colors.background)
+//    ) {
+//        Column(){
+//            Header()
+//
+//            BottomNavigationBar(navController = navController )
+//
+//        }
+//    }
+}
