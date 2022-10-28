dad2916eff410ecea56d0b4e4daaf6020d7d8d86
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-28 12:51:30 +0300

    create common profile page

---
+++ app/src/main/java/com/example/recordily_client/pages/common/CommonProfilePage.kt
@@ -0,0 +1,27 @@
+package com.example.recordily_client.pages.common
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.navigation.NavController
+import com.example.recordily_client.components.BottomNavigationBar
+import com.example.recordily_client.components.Header
+import com.example.recordily_client.components.SongsBox
+import com.example.recordily_client.components.TopNavBar
+
+@Composable
+fun CommonProfilePage(navController: NavController){
+    Scaffold(
+        topBar = { Header(navController) },
+        bottomBar = { BottomNavigationBar(navController) }
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            TopNavBar(currentPage = "Home")
+
+            SongsBox("Suggested Songs")
+        }
+    }
+}
