3266d9259f53783ed0855dc785919dff9c941595
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-28 12:07:46 +0300

    design song box component

--- app/src/main/java/com/example/recordily_client/components/SongsBox.kt
+++ app/src/main/java/com/example/recordily_client/components/SongsBox.kt
@@ -1,23 +1,44 @@
 package com.example.recordily_client.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.recordily_client.R
 
 @Composable
 fun SongsBox(title: String){
-    Column{
+    Column(
+        modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.padding_large))
+    ){
         Text(
             text = title,
             fontWeight = FontWeight.ExtraBold,
-            fontSize = dimensionResource(id = R.dimen.font_medium).value.sp
+            fontSize = dimensionResource(id = R.dimen.font_medium).value.sp,
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium))
         )
-        Row(){
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .height(195.dp)
+                .shadow(25.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(MaterialTheme.colors.surface)
+        ){
 
         }
     }
