55c378c0e112ea5694659290ebccc49a53186dfd
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-29 21:34:24 +0300

    frontend: apply bounce effect on button

--- recordily_client/app/src/main/java/com/example/recordily_client/components/RoundButton.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/components/RoundButton.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -105,12 +107,15 @@ fun LargeRoundButton(text:String, onClick: () -> (Unit)){
 fun SmallTealButton(text:String, onClick: () -> (Unit)){
     Button(
         modifier = Modifier
+            .padding(dimensionResource(id = R.dimen.padding_small))
+            .bounceClick()
             .height(35.dp)
             .width(80.dp)
             .border(1.dp, MaterialTheme.colors.secondary, RoundedCornerShape(20.dp)),
         colors = ButtonDefaults.buttonColors(
             backgroundColor = Color.Transparent
         ),
+        interactionSource = remember { NoRippleInteractionSource() },
         elevation = ButtonDefaults.elevation(
             defaultElevation = 0.dp,
         ),
