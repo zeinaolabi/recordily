9d4e40b71957a79b7e3c7f563705e65eecac954e
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-29 22:05:15 +0300

    frontend: create songs stats page

---
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/artist/SongsStatsPage.kt
@@ -0,0 +1,8 @@
+package com.example.recordily_client.pages.artist
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SongsStatsPage(navController: NavController){
+}
