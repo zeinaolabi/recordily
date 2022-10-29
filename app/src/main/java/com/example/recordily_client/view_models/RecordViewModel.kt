622871546c3541b2a7d0adb231a4fe23510ed358
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-29 10:48:37 +0300

    frontend: create record view model

---
+++ app/src/main/java/com/example/recordily_client/view_models/RecordViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.recordily_client.view_models
+
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.app.Activity
+import android.app.Application
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.os.Build
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.AndroidViewModel
+import java.io.IOException
+import java.io.File
+
+
+
+
+@SuppressLint("StaticFieldLeak")
+class RecordViewModel(application: Application): AndroidViewModel(application) {
+
+
+}
