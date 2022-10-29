46f9f47073f9651c762687b42731aa1f70ef6fcc
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-29 21:57:57 +0300

    frontend: create views stats page

---
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/artist/ViewsStatsPage.kt
@@ -0,0 +1,8 @@
+package com.example.recordily_client.pages.artist
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ViewsStatsPage(navController: NavController){
+}
