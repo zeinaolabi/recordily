8b7cf26f8946e5a6aa8a4f62cfcc1aeaaf7ace29
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-30 13:52:54 +0200

    frontend: add artists cards in artists page

--- recordily_client/app/src/main/java/com/example/recordily_client/pages/common/ArtistsPage.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/common/ArtistsPage.kt
@@ -1,8 +1,76 @@
 package com.example.recordily_client.pages.common
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.recordily_client.R
+import com.example.recordily_client.components.*
+import com.example.recordily_client.navigation.Destination
+import com.example.recordily_client.navigation.Screen
+
+private val searchInput = mutableStateOf("")
 
 @Composable
 fun ArtistsPage(navController: NavController) {
+    Scaffold(
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = dimensionResource(id = R.dimen.padding_large)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ){
+                ArtistsPageContent(navController)
+            }
+        },
+        bottomBar = { BottomNavigationBar(navController) }
+    )
+}
+
+@Composable
+fun ArtistsPageContent(navController: NavController){
+    val likes = Destination(stringResource(R.string.likes), Screen.LibraryPage.route)
+    val playlists = Destination(stringResource(R.string.playlists), Screen.PlaylistsPage.route)
+    val artists = Destination(stringResource(R.string.artists), Screen.ArtistsPage.route)
+    val pageOptions = listOf(
+        likes, playlists, artists
+    )
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ){
+        LibraryHeader(
+            input = searchInput,
+            navController = navController
+        )
+
+        TopNavBar(
+            pageOptions = pageOptions,
+            currentPage = "Artists",
+            navController = navController
+        )
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(dimensionResource(id = R.dimen.padding_medium))
+            .verticalScroll(rememberScrollState())
+    ){
+        for(i in 1..3){
+            ArtistCard(navController)
+        }
+    }
 }
