e21711be61317c1d68acb55e9c5d5b134115b70e
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-30 18:29:06 +0200

    frontend: create artist profile page

--- recordily_client/app/src/main/java/com/example/recordily_client/pages/common/ArtistProfilePage.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/common/ArtistProfilePage.kt
@@ -1,2 +1,69 @@
 package com.example.recordily_client.pages.common
 
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.dimensionResource
+import androidx.navigation.NavController
+import com.example.recordily_client.R
+import com.example.recordily_client.components.*
+
+private val popUpVisibility = mutableStateOf(false)
+
+@ExperimentalAnimationApi
+@Composable
+fun ArtistProfilePage(navController: NavController){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            ArtistPageHeader(navController)
+
+            HorizontalLine()
+
+            ArtistProfileContent(navController)
+
+        }
+
+        AnimatedVisibility(
+            visible = popUpVisibility.value,
+            enter = expandVertically(expandFrom = Alignment.CenterVertically),
+            exit = shrinkVertically(shrinkTowards = Alignment.CenterVertically)
+        ) {
+            Popup(popUpVisibility)
+        }
+    }
+}
+
+@Composable
+fun ArtistProfileContent(navController: NavController){
+    Column(
+        modifier = Modifier
+            .padding(bottom = dimensionResource(id = R.dimen.padding_very_large))
+            .padding(horizontal = dimensionResource(id = R.dimen.padding_medium))
+            .verticalScroll(rememberScrollState())
+    ){
+        for(i in 1..3){
+            SongCard(navController){
+                popUpVisibility.value = true
+            }
+        }
+    }
+}
