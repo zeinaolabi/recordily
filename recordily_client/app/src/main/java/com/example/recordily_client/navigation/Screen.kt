43da473018d8fd900749c451de055450f0e385ef
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-30 11:53:39 +0200

    frontend: create library page screen

--- recordily_client/app/src/main/java/com/example/recordily_client/navigation/Screen.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/navigation/Screen.kt
@@ -8,9 +8,10 @@ sealed class Screen(val route: String){
     object UnreleasedPage: Screen(route = "unreleased_page")
     object RecordPage: Screen(route = "record_page")
     object ViewsStatsPage: Screen(route = "views_stats_page")
-    object SongsStatsPage: Screen(route = "views_stats_page")
+    object SongsStatsPage: Screen(route = "songs_stats_page")
     object CommonSearchPage: Screen(route = "common_search_page")
     object CommonLiveEventsPage: Screen(route = "common_live_page")
+    object LibraryPage: Screen(route = "library_page")
 
 
 }
