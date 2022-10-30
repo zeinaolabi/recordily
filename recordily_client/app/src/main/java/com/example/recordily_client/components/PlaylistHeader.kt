d2da8df8ea91fd0c58310455cef35119c7e0053d
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-30 14:37:01 +0200

    frontend: create playlist header component

---
+++ recordily_client/app/src/main/java/com/example/recordily_client/components/PlaylistHeader.kt
@@ -0,0 +1,73 @@
+package com.example.recordily_client.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.recordily_client.R
+
+@Composable
+fun PlaylistHeader(navController: NavController){
+    Row(
+        modifier = Modifier.padding(
+            vertical = dimensionResource(id = R.dimen.padding_large),
+            horizontal = dimensionResource(id = R.dimen.padding_medium)
+        )
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.recordily_dark_logo),
+            contentDescription = "playlist picture",
+            modifier = Modifier
+                .size(110.dp)
+                .clip(CircleShape)
+                .border(3.dp, MaterialTheme.colors.secondary, CircleShape),
+            contentScale = ContentScale.Crop
+        )
+
+        PlaylistHeaderContent(navController)
+    }
+}
+
+@Composable
+fun PlaylistHeaderContent(navController: NavController){
+    Column(
+        modifier = Modifier
+            .height(110.dp)
+            .padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.SpaceBetween
+    ){
+
+        Text(
+            text = "Playlist Title",
+            fontSize = dimensionResource(id = R.dimen.font_medium).value.sp,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium))
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            MediumRoundButton(text = "Edit Playlist", onClick = {})
+        }
+    }
+}
