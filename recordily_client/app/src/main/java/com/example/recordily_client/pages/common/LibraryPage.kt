45bdc3e53b8d88aca132be72cf04a5fb2aacd95d
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-30 12:32:58 +0200

    frontend: add navigation to playlists from library page

--- recordily_client/app/src/main/java/com/example/recordily_client/pages/common/LibraryPage.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/common/LibraryPage.kt
@@ -1,28 +1,16 @@
 package com.example.recordily_client.pages.common
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.recordily_client.R
-import com.example.recordily_client.SearchTextField
 import com.example.recordily_client.components.*
 import com.example.recordily_client.navigation.Destination
 import com.example.recordily_client.navigation.Screen
@@ -49,8 +37,8 @@ fun LibraryPage(navController: NavController){
 
 @Composable
 fun LibraryPageContent(navController: NavController){
-    val likes = Destination(stringResource(R.string.likes), Screen.CommonLandingPage.route)
-    val playlists = Destination(stringResource(R.string.playlists), Screen.ViewsStatsPage.route)
+    val likes = Destination(stringResource(R.string.likes), Screen.LibraryPage.route)
+    val playlists = Destination(stringResource(R.string.playlists), Screen.PlaylistsPage.route)
     val artists = Destination(stringResource(R.string.artists), Screen.SongsStatsPage.route)
     val pageOptions = listOf(
         likes, playlists, artists
@@ -79,6 +67,4 @@ fun LibraryPageContent(navController: NavController){
             SongCard(navController)
         }
     }
-
-
 }
