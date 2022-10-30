1dca44815d233d2342c503cc300f50c489d69153
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-30 13:07:46 +0200

    frontend: create artist page

---
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/common/ArtistsPage.kt
@@ -0,0 +1,8 @@
+package com.example.recordily_client.pages.common
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ArtistsPage(navController: NavController) {
+}
