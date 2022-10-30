21554d6a6ca6b6ecd7a41dfcd36306a4850d5466
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-30 16:36:39 +0200

    frontend: add pop up to playlist page

--- recordily_client/app/src/main/java/com/example/recordily_client/pages/common/PlaylistPage.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/common/PlaylistPage.kt
@@ -1,55 +1,80 @@
 package com.example.recordily_client.pages.common
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.recordily_client.R
 import com.example.recordily_client.components.*
 
+private val popUpVisibility = mutableStateOf(false)
 
+@ExperimentalAnimationApi
 @Composable
 fun PlaylistPage(navController: NavController){
-    Scaffold(
-        topBar = { PlaylistHeader(navController) },
-        content = {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            PlaylistHeader(navController)
 
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = dimensionResource(id = R.dimen.padding_large))
-                        .drawBehind {
-                            val strokeWidth = Stroke.DefaultMiter * 1.5f
-                            val y = size.height - strokeWidth / 10 + 20
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = dimensionResource(id = R.dimen.padding_large))
+                    .drawBehind {
+                        val strokeWidth = Stroke.DefaultMiter * 1.5f
+                        val y = size.height - strokeWidth / 10 + 20
 
-                            drawLine(
-                                Color.Black,
-                                Offset(1f, y),
-                                Offset(size.width, y),
-                                strokeWidth,
-                                cap = StrokeCap.Round
-                            )
-                        }
-                ){}
+                        drawLine(
+                            Color.Black,
+                            Offset(1f, y),
+                            Offset(size.width, y),
+                            strokeWidth,
+                            cap = StrokeCap.Round
+                        )
+                    }
+            ){}
 
-                PlaylistPageContent(navController)
-            }
+            PlaylistPageContent(navController)
+
         }
-    )
+
+        AnimatedVisibility(
+            visible = popUpVisibility.value,
+            enter = expandVertically(expandFrom = Alignment.CenterVertically),
+            exit = shrinkVertically(shrinkTowards = Alignment.CenterVertically)
+        ) {
+            Popup(popUpVisibility)
+        }
+    }
 }
 
 @Composable
@@ -61,7 +86,9 @@ fun PlaylistPageContent(navController: NavController){
             .verticalScroll(rememberScrollState())
     ){
         for(i in 1..3){
-            SongCard(navController)
+            SongCard(navController){
+                popUpVisibility.value = true
+            }
         }
     }
 }
