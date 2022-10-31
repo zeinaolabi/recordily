ba553a211709f14b5647dd45c263b70b276a37aa
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-31 17:42:26 +0200

    frontend: create suggested songs page screen

--- recordily_client/app/src/main/java/com/example/recordily_client/navigation/Screen.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/navigation/Screen.kt
@@ -3,14 +3,14 @@ package com.example.recordily_client.navigation
 sealed class Screen(val route: String){
     object LoginPage: Screen(route = "login_screen")
     object RegistrationPage: Screen(route = "registration_screen")
-    object CommonLandingPage: Screen(route = "common_landing_page")
-    object CommonProfilePage: Screen(route = "common_profile_page")
+    object LandingPage: Screen(route = "common_landing_page")
+    object ProfilePage: Screen(route = "common_profile_page")
     object UnreleasedPage: Screen(route = "unreleased_page")
     object RecordPage: Screen(route = "record_page")
     object ViewsStatsPage: Screen(route = "views_stats_page")
     object SongsStatsPage: Screen(route = "songs_stats_page")
-    object CommonSearchPage: Screen(route = "common_search_page")
-    object CommonLiveEventsPage: Screen(route = "common_live_page")
+    object SearchPage: Screen(route = "common_search_page")
+    object LiveEventsPage: Screen(route = "common_live_page")
     object LibraryPage: Screen(route = "library_page")
     object PlaylistsPage: Screen(route = "playlists_page")
     object ArtistsPage: Screen(route = "artists_page")
@@ -21,5 +21,6 @@ sealed class Screen(val route: String){
     object EditProfilePage: Screen(route = "edit_profile_page")
     object SongPage: Screen(route = "song_page")
     object SongStatsPage: Screen(route = "song_stats_page")
+    object SuggestedSongsPage: Screen(route = "song_stats_page")
 
 }
