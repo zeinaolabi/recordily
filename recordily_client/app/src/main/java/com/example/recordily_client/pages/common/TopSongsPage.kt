dc6ea25bf4c6c742e295ab919a60814f42393c01
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-31 23:03:19 +0200

    frontend: finalize top songs page design

--- recordily_client/app/src/main/java/com/example/recordily_client/pages/common/TopSongsPage.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/common/TopSongsPage.kt
@@ -1,26 +1,24 @@
 package com.example.recordily_client.pages.common
 
-import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.recordily_client.R
-import com.example.recordily_client.components.*
+import com.example.recordily_client.components.ExitBar
+import com.example.recordily_client.components.HorizontalLine
+import com.example.recordily_client.components.Popup
+import com.example.recordily_client.components.SongCard
 import com.example.recordily_client.navigation.Screen
 import com.example.recordily_client.navigation.navigateTo
 
@@ -33,17 +31,22 @@ fun TopSongsPage(navController: NavController){
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.background)
-    ){
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            ExitBar(navController, stringResource(id = R.string.top_songs))
+    ) {
+        Column(modifier = Modifier.fillMaxSize()){
+            ExitBar(
+                navController = navController,
+                title = stringResource(id = R.string.top_songs)
+            )
 
-            HorizontalLine()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = dimensionResource(id = R.dimen.padding_medium)),
+            ){
+                HorizontalLine()
 
-            TopSongsContent(navController)
-
+                TopSongsContent(navController)
+            }
         }
 
         AnimatedVisibility(
@@ -56,7 +59,6 @@ fun TopSongsPage(navController: NavController){
                 isPlaylist = false
             )
         }
-
     }
 }
 
@@ -73,11 +75,12 @@ private fun TopSongsContent(navController: NavController){
                     navigateTo(
                         navController = navController,
                         destination = Screen.SongPage.route,
-                        popUpTo = Screen.AlbumPage.route
+                        popUpTo = Screen.SuggestedSongsPage.route
                     )
                 },
                 onMoreClick = { popUpVisibility.value = true }
             )
         }
+
     }
 }
