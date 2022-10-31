7d3769e891e4910d20fb41b7d199b547a71f1276
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-31 18:49:22 +0200

    frontend: create live event dialog

---
+++ recordily_client/app/src/main/java/com/example/recordily_client/components/StartLiveEventDialog.kt
@@ -0,0 +1,64 @@
+package com.example.recordily_client.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.recordily_client.R
+
+@Composable
+fun StartLiveEventDialog(isOpen: MutableState<Boolean>, eventName: MutableState<String>){
+    AlertDialog(
+        onDismissRequest = {
+            isOpen.value = false
+        },
+        backgroundColor = MaterialTheme.colors.surface,
+        title = {
+            Text(
+                text = stringResource(id = R.string.start_event),
+                fontSize = dimensionResource(id = R.dimen.font_medium).value.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colors.onPrimary,
+                modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_large))
+            )
+        },
+        text = {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                TextField(
+                    input = eventName,
+                    text = stringResource(id = R.string.event_name),
+                    visibility = true
+                )
+            }
+
+        },
+        confirmButton = {
+            Row(
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            ){
+                SmallRoundButton(
+                    text = stringResource(id = R.string.start),
+                    onClick = {
+                        //Navigate to event page
+                        isOpen.value = false
+                    }
+                )
+            }
+        }
+    )
+}
