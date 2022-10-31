eafbdf13520401c7f026861e730d51c27bcd216a
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-31 18:12:21 +0200

    frontend: finalize suggest songs page design

--- recordily_client/app/src/main/java/com/example/recordily_client/pages/common/SuggestedSongsPage.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/common/SuggestedSongsPage.kt
@@ -1,11 +1,16 @@
 package com.example.recordily_client.pages.common
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -13,25 +18,73 @@ import androidx.navigation.NavController
 import com.example.recordily_client.R
 import com.example.recordily_client.components.ExitBar
 import com.example.recordily_client.components.HorizontalLine
+import com.example.recordily_client.components.Popup
+import com.example.recordily_client.components.SongCard
+import com.example.recordily_client.navigation.Screen
 
+private val popUpVisibility = mutableStateOf(false)
+
+@ExperimentalAnimationApi
 @Composable
 fun SuggestedSongsPage(navController: NavController){
-    Scaffold(
-        topBar = { ExitBar(
-            navController = navController,
-            title = stringResource(id = R.string.song_statistics)
-        )
-        },
-        content = {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+    ) {
+        Column(modifier = Modifier.fillMaxSize()){
+            ExitBar(
+                navController = navController,
+                title = stringResource(id = R.string.suggested)
+            )
+
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+                    .padding(top = dimensionResource(id = R.dimen.padding_medium)),
             ){
                 HorizontalLine()
 
+                SuggestedSongsContent(navController)
             }
         }
-    )
+
+        AnimatedVisibility(
+            visible = popUpVisibility.value,
+            enter = expandVertically(expandFrom = Alignment.CenterVertically),
+            exit = shrinkVertically(shrinkTowards = Alignment.Bottom)
+        ) {
+            Popup(
+                popUpVisibility = popUpVisibility,
+                isPlaylist = false
+            )
+        }
+    }
+}
+
+@Composable
+private fun SuggestedSongsContent(navController: NavController){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.padding_medium)),
+    ){
+        for(i in 1..3){
+            SongCard(
+                onSongClick = {
+                    navController.navigate(Screen.SongPage.route) {
+
+                        popUpTo(Screen.SuggestedSongsPage.route) {
+                            saveState = true
+                        }
+
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                onMoreClick = { popUpVisibility.value = true }
+            )
+        }
+
+    }
 }
