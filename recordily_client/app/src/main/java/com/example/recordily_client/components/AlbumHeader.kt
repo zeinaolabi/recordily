037fe732c6add41252da33f36019755895f0e68a
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-31 20:50:50 +0200

    frontend: create album header component

---
+++ recordily_client/app/src/main/java/com/example/recordily_client/components/AlbumHeader.kt
@@ -0,0 +1,69 @@
+package com.example.recordily_client.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.recordily_client.R
+
+@Composable
+fun AlbumHeader(){
+    Row(
+        modifier = Modifier.padding(
+            vertical = dimensionResource(id = R.dimen.padding_large),
+            horizontal = dimensionResource(id = R.dimen.padding_medium)
+        )
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.recordily_dark_logo),
+            contentDescription = "album picture",
+            modifier = Modifier
+                .size(110.dp)
+                .clip(CircleShape)
+                .border(3.dp, MaterialTheme.colors.secondary, CircleShape),
+            contentScale = ContentScale.Crop
+        )
+
+        AlbumHeaderContent()
+    }
+}
+
+@Composable
+private fun AlbumHeaderContent(){
+    Column(
+        modifier = Modifier
+            .height(110.dp)
+            .padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.SpaceBetween
+    ){
+
+        Column{
+            Text(
+                text = "Album name",
+                fontSize = dimensionResource(id = R.dimen.font_medium).value.sp,
+                fontWeight = FontWeight.ExtraBold,
+                modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_small)),
+                color = MaterialTheme.colors.onPrimary
+            )
+
+            Text(
+                text = "Artist name",
+                fontSize = dimensionResource(id = R.dimen.font_very_small).value.sp,
+                fontWeight = FontWeight.Medium,
+                color = MaterialTheme.colors.onPrimary
+            )
+        }
+
+    }
+}
