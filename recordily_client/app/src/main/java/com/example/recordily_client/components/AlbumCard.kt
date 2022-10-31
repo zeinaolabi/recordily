7cb9a00edcbbd759c6c0b9fb0cb9419ac9e6f64c
Zeina Olabi <zeina.olabi02@gmail.com>
2022-10-31 20:43:24 +0200

    frontend: add albums card with button component

--- recordily_client/app/src/main/java/com/example/recordily_client/components/AlbumCard.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/components/AlbumCard.kt
@@ -1,12 +1,12 @@
 package com.example.recordily_client.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -14,6 +14,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,7 +22,46 @@ import androidx.navigation.NavController
 import com.example.recordily_client.R
 
 @Composable
-fun AlbumCard(navController: NavController){
+fun AlbumsCards(title: String, navController: NavController, destination: String, onAlbumClick: ()->(Unit),onMoreClick: () -> (Unit)){
+    Column(
+        modifier = Modifier.padding(bottom= dimensionResource(id = R.dimen.padding_medium))
+    ){
+        Text(
+            text = title,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = dimensionResource(id = R.dimen.font_medium).value.sp,
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium)),
+            color = MaterialTheme.colors.onPrimary
+        )
+
+        AlbumCardContent(navController, destination, onAlbumClick ,onMoreClick)
+    }
+}
+
+@Composable
+private fun AlbumCardContent(navController: NavController, destination: String, onAlbumClick: ()->(Unit), onMoreClick: () -> (Unit)){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(260.dp)
+            .verticalScroll(ScrollState(0)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        for(i in 1..3){
+            AlbumCard(onAlbumClick)
+        }
+
+        SmallTealButton(
+            text = stringResource(id = R.string.more),
+            onClick = {
+                navController.navigate(destination)
+            })
+
+    }
+}
+
+@Composable
+fun AlbumCard(onAlbumClick: ()->(Unit)){
     Row(
         modifier = Modifier
             .padding(vertical = dimensionResource(id = R.dimen.padding_small))
@@ -32,17 +72,27 @@ fun AlbumCard(navController: NavController){
             .padding(horizontal = dimensionResource(id = R.dimen.padding_medium)),
         verticalAlignment = Alignment.CenterVertically
     ){
-        AlbumCardContent()
+        AlbumCardContent(onAlbumClick)
     }
 }
 
 @Composable
-fun AlbumCardContent(){
-    Row(verticalAlignment = Alignment.CenterVertically)
+private fun AlbumCardContent(onAlbumClick: () -> (Unit)){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth(0.9f)
+            .clickable(
+                interactionSource = remember { NoRippleInteractionSource() },
+                indication = null
+            ) {
+                onAlbumClick()
+            }
+    )
     {
         Image(
             painter = painterResource(R.drawable.recordily_dark_logo),
-            contentDescription = "logo",
+            contentDescription = "album picture",
             modifier = Modifier
                 .size(50.dp)
                 .clip(CircleShape),
