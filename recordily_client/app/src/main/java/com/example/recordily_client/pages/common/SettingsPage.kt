eff3183defb3787ea2278ec9fc7391d727786b31
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-01 14:36:54 +0200

    frontend: add logout button in settings page

--- recordily_client/app/src/main/java/com/example/recordily_client/pages/common/SettingsPage.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/common/SettingsPage.kt
@@ -2,21 +2,21 @@ package com.example.recordily_client.pages.common
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.recordily_client.R
 import com.example.recordily_client.components.*
+import com.example.recordily_client.navigation.Screen
+import com.example.recordily_client.navigation.navigateTo
+import com.example.recordily_client.view_models.LoginViewModel
 
 @Composable
 fun SettingsPage(navController: NavController) {
@@ -35,7 +35,7 @@ fun SettingsPage(navController: NavController) {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large))
                 ){
-                    SettingsContent()
+                    SettingsContent(navController)
                 }
             }
         }
@@ -43,7 +43,9 @@ fun SettingsPage(navController: NavController) {
 }
 
 @Composable
-private fun SettingsContent(){
+private fun SettingsContent(navController: NavController){
+    val loginViewModel : LoginViewModel = viewModel()
+
     Text(
         text = stringResource(id = R.string.edit_profile),
         fontSize = dimensionResource(id = R.dimen.font_title).value.sp,
@@ -51,13 +53,19 @@ private fun SettingsContent(){
         color = MaterialTheme.colors.onPrimary
     )
 
-    Image(
-        painter = painterResource(id = R.drawable.profile_picture),
-        contentDescription = "logo",
-        modifier = Modifier
-            .size(160.dp)
-            .clip(CircleShape)
-            .border(2.dp, MaterialTheme.colors.secondary, CircleShape)
+    Text(
+        text = stringResource(id = R.string.logout),
+        fontSize = dimensionResource(id = R.dimen.font_title).value.sp,
+        fontWeight = FontWeight.ExtraBold,
+        color = MaterialTheme.colors.onPrimary,
+        modifier = Modifier.clickable{
+            loginViewModel.logout()
+            navigateTo(
+                navController = navController,
+                destination = Screen.LoginPage.route,
+                popUpTo = Screen.LoginPage.route
+            )
+        }
     )
 
 }
