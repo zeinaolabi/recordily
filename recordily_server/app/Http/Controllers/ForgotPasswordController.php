9b1aa556f1939ebb8790e5d7198260d09c726d55
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-03 15:55:38 +0200

    backend: create forgot password controller

---
+++ recordily_server/app/Http/Controllers/ForgotPasswordController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ForgotPasswordRequest;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
+class ForgotPasswordController extends Controller
+{
+
+}
