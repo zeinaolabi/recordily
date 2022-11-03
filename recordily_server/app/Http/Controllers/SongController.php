bf869bf38818ec46c8a3626c80149e90bbb70bd3
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-03 15:58:58 +0200

    backend: create upload song function in song controller

--- recordily_server/app/Http/Controllers/SongController.php
+++ recordily_server/app/Http/Controllers/SongController.php
@@ -14,5 +14,42 @@ use wapmorgan\Mp3Info\Mp3Info;
 
 class SongController extends Controller
 {
+    function uploadSong(UploadSongRequest $request): bool{
+        $path = public_path() . '/chunks/';
+
+//        if (! $request->hasFile('audio')) {
+//            return response()->json([
+//                "status" => "File Not Found"
+//            ], 400);
+//        }
+
+        try {
+            $song = $request->file;
+            $bString = hex2bin($song);
+            $song_path = $path . uniqid();
+            file_put_contents($song_path, $bString);
+//            $audio = new Mp3Info($song, true);
+        }catch (Exception $e) {
+            return false;
+        }
+
+//        Song::create([
+//            'name' => $request->name,
+//            'picture' => $request->picture,
+//            'path' => $song_path,
+//            'size' => $song->getSize(),
+//            'type' => $song->extension(),
+//            'time_length' => $audio->duration,
+//            'user_id' => Auth::id(),
+//            'album_id' => $request->album_id
+//        ]);
+
+        return true;
+//        return response()->json([
+//            "status" => "Successfully saved"
+//        ], 201);
+    }
+
+
 
 }
