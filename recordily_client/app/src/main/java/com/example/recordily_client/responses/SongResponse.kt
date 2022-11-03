c4aab322e83ec9862c0417be00dd2d89ff799ca8
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-03 20:13:58 +0200

    frontend: create song response

---
+++ recordily_client/app/src/main/java/com/example/recordily_client/responses/SongResponse.kt
@@ -0,0 +1,15 @@
+package com.example.recordily_client.responses
+
+data class SongResponse(
+    val album_id: Int,
+    val created_at: String,
+    val id: Int,
+    val name: String,
+    val path: String,
+    val picture: String,
+    val size: Int,
+    val time_length: Int,
+    val type: String,
+    val updated_at: String,
+    val user_id: Int
+)
