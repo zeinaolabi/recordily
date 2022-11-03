d6a5aeee15df6e1a7f98a71090ca079f98c57e2e
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-03 20:18:49 +0200

    frontend: add upload song service instead of repository

--- recordily_client/app/src/main/java/com/example/recordily_client/view_models/UploadSongViewModel.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/view_models/UploadSongViewModel.kt
@@ -5,20 +5,19 @@ import android.app.Application
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.recordily_client.repositories.UploadSongRepository
 import com.example.recordily_client.requests.UploadSongRequest
+import com.example.recordily_client.services.SongService
 import kotlinx.coroutines.launch
-import java.io.ByteArrayOutputStream
 
 @SuppressLint("StaticFieldLeak")
 class UploadSongViewModel(application: Application): AndroidViewModel(application) {
 
     val context: Context = getApplication<Application>().applicationContext
-    private val repository = UploadSongRepository()
+    private val service = SongService()
 
     fun uploadSong(uploadSongRequest: UploadSongRequest){
         viewModelScope.launch {
-            repository.uploadSong(uploadSongRequest)
+            service.uploadSong(uploadSongRequest)
         }
     }
 }
