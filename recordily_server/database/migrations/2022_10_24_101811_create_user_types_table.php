834407c749f6a0a9e9faab36e5d89ed03ba6230c
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-03 15:47:30 +0200

    backend: make type unique in user types table

--- recordily_server/database/migrations/2022_10_24_101811_create_user_types_table.php
+++ recordily_server/database/migrations/2022_10_24_101811_create_user_types_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('user_types', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
+            $table->string('type')->unique();
             $table->timestamps();
         });
     }
