4bd33bddad15011c2116d55d064f1723c79c719a
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-03 20:31:16 +0200

    backend: add mail configuration

--- recordily_server/config/mail.php
+++ recordily_server/config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'recordily@anonymous.com'),
+        'name' => env('MAIL_FROM_NAME', 'Recordily'),
     ],
 
     /*
@@ -114,5 +114,13 @@ return [
             resource_path('views/vendor/mail'),
         ],
     ],
+
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ]
 
 ];
