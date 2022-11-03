0d38fd2915cef8b87e58bff2c1097e29d2c10137
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-03 20:36:50 +0200

    backend: save file chunks in uploads folder

--- recordily_server/app/Http/Controllers/SongController.php
+++ recordily_server/app/Http/Controllers/SongController.php
@@ -6,32 +6,66 @@ use App\Http\Requests\UploadSongRequest;
 use App\Models\Like;
 use App\Models\Play;
 use App\Models\Song;
+use DirectoryIterator;
 use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use wapmorgan\Mp3Info\Mp3Info;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class SongController extends Controller
 {
     function uploadSong(UploadSongRequest $request): bool{
-        $path = public_path() . '/chunks/';
-
 //        if (! $request->hasFile('audio')) {
 //            return response()->json([
 //                "status" => "File Not Found"
 //            ], 400);
 //        }
 
-        try {
-            $song = $request->file;
-            $bString = hex2bin($song);
-            $song_path = $path . uniqid();
-            file_put_contents($song_path, $bString);
-//            $audio = new Mp3Info($song, true);
-        }catch (Exception $e) {
-            return false;
+        $path = public_path() . '/uploads/' . $request->user_id . '/';
+
+        if(!File::exists($path)){
+            File::makeDirectory($path,0775, true);
         }
+
+//        try {
+//            $song = $request->file;
+//            $song_path = $path . '/' . $request->chunk_num;
+//            file_put_contents($song_path, $song);
+////            $audio = new Mp3Info($song, true);
+//        }catch (Exception $e) {
+//            return false;
+//        }
+
+//        $chunks = Storage::disk('public')->allFiles("uploads/" . $request->user_id . '/');
+//        var_dump($chunks);
+
+//        $chunks = new DirectoryIterator($path);
+//        var_dump(count($chunks));
+//        foreach ($chunks as $chunk) {
+//            var_dump($chunk['pathName']);
+//        }
+
+        $chunks = Storage::disk('uploads')->files($request->user_id);
+
+        if(count($chunks) == $request->chunk_size){
+            $files = [];
+            for($i =0; $i < count($chunks); $i++){
+                $files[] = file_get_contents($path . $i);
+            }
+            file_put_contents($path . '/'. uniqid(), $files);
+        }
+
+
+
+//        if(count($chunks) == $request->chunk_size){
+//            $audioFile = "";
+//            foreach ($chunks as $chunk){
+//                var_dump($chunks);
+//            }
+//            var_dump($chunk);
+//            file_put_contents($path . '/'. uniqid(), $audioFile);
+//        }
 
 //        Song::create([
 //            'name' => $request->name,
