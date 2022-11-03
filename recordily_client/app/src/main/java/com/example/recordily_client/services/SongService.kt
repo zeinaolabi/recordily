92ddf00cc353bc3d36f742e5e3f8d954e009da24
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-03 21:59:57 +0200

    frontend: create get suggested function in song service

--- recordily_client/app/src/main/java/com/example/recordily_client/services/SongService.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/services/SongService.kt
@@ -3,17 +3,15 @@ package com.example.recordily_client.services
 import com.example.recordily_client.apis.RetrofitInstance
 import com.example.recordily_client.requests.UploadSongRequest
 import com.example.recordily_client.responses.SongResponse
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
-import java.io.File
 
 class SongService {
     suspend fun uploadSong(uploadSongRequest: UploadSongRequest): Boolean {
-        return RetrofitInstance.uploadSong.uploadSong(uploadSongRequest)
+        return RetrofitInstance.songApi.uploadSong(uploadSongRequest)
     }
 
-    suspend fun getTopPlayed(): List<SongResponse>
-    {
-        return RetrofitInstance.songApi.topSongs()
+    suspend fun getSuggested(limit: Int): List<SongResponse>{
+        return RetrofitInstance.songApi.suggestedSongs(limit)
     }
+
+
 }
