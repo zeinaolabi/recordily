36097765daaef5ed5fbd18ff2d885421143fa640
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-03 15:58:28 +0200

    backend: create song controller

---
+++ recordily_server/app/Http/Controllers/SongController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UploadSongRequest;
+use App\Models\Like;
+use App\Models\Play;
+use App\Models\Song;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use wapmorgan\Mp3Info\Mp3Info;
+
+class SongController extends Controller
+{
+
+}
