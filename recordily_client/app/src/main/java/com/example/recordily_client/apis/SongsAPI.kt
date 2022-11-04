a6932edd8f9ffdd4368b7c8169dc13b4878183ff
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-04 16:33:28 +0200

    frontend: create get liked songs api

--- recordily_client/app/src/main/java/com/example/recordily_client/apis/SongsAPI.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/apis/SongsAPI.kt
@@ -3,10 +3,7 @@ package com.example.recordily_client.apis
 import com.example.recordily_client.requests.UploadSongRequest
 import com.example.recordily_client.responses.SearchResponse
 import com.example.recordily_client.responses.SongResponse
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface SongsAPI {
     @GET("suggested_songs/{limit}")
@@ -23,6 +20,9 @@ interface SongsAPI {
 
     @GET("search/{input}")
     suspend fun searchForSong(@Path("input") input: String): SearchResponse
+
+    @GET("liked_songs")
+    suspend fun getLikedSongs(@Header("Authorization") token: String): List<SongResponse>
 
     @POST("upload_song")
     suspend fun uploadSong(@Body uploadSongRequest: UploadSongRequest): Boolean
