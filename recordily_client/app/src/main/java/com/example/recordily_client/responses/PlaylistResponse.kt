ea66dd8a9997d0f5d1a7ffb6eb9883118780fa7c
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-05 21:38:50 +0200

    frontend: create playlist response

---
+++ recordily_client/app/src/main/java/com/example/recordily_client/responses/PlaylistResponse.kt
@@ -0,0 +1,10 @@
+package com.example.recordily_client.responses
+
+data class PlaylistResponse(
+    val created_at: String,
+    val id: Int,
+    val name: String,
+    val picture: String,
+    val updated_at: String,
+    val user_id: Int
+)
