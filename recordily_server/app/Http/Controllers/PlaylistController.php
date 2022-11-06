c3c6c29de9f83bcba30e7e1457f6387918a459ff
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-06 17:31:55 +0200

    backend: create get playlist info api

--- recordily_server/app/Http/Controllers/PlaylistController.php
+++ recordily_server/app/Http/Controllers/PlaylistController.php
@@ -10,14 +10,19 @@ use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\URL;
+use Psy\Util\Json;
 
 class PlaylistController extends Controller
 {
     public function getPlaylists(): JsonResponse
     {
         $id = Auth::id();
-
         $playlists = Playlist::where('user_id', $id)->get();
+
+        foreach ($playlists as $playlist) {
+            $playlist->picture = URL::to($playlist->picture);
+        }
 
         return response()->json($playlists);
     }
@@ -31,11 +36,24 @@ class PlaylistController extends Controller
         return response()->json($result);
     }
 
+    public function getPlaylistInfo(int $playlist_id): JsonResponse
+    {
+        $playlist = Playlist::find($playlist_id);
+
+        if (!$playlist) {
+            return response()->json("Playlist not found", 204);
+        }
+
+        $playlist->picture = URL::to($playlist->picture);
+
+        return response()->json($playlist);
+    }
+
     public function addPlaylist(PlaylistRequest $request): JsonResponse
     {
         $id = Auth::id();
 
-        $path = public_path() . '/images/' . $id . '/';
+        $path = public_path();
 
         if (!File::exists($path)) {
             File::makeDirectory($path);
@@ -44,13 +62,15 @@ class PlaylistController extends Controller
         try {
             $picture = $request->file('picture');
 
-            $picture_path = $path . uniqid() . '.' . $picture->extension();
-            file_put_contents($picture_path, $picture->getContent());
+            $picture_path = '/images/' . $id . '/' . uniqid() . '.' . $picture->extension();
+            file_put_contents($path . $picture_path, $picture->getContent());
         } catch (Exception $e) {
             return response()->json(['error' => $e], 400);
         }
 
-        if(!Playlist::createPlaylist($id, $request->name, $picture_path)){
+        $playlist_name = str_replace('"', '', $request->get('name'));
+
+        if (!Playlist::createPlaylist($id, $playlist_name, $picture_path)) {
             return response()->json('unsuccessfully attempt', 400);
         }
 
