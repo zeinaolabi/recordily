acf06798128beacced1128e83ab9ecd633de6440
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-06 11:39:45 +0200

    backend: create playlist request

---
+++ recordily_server/app/Http/Requests/PlaylistRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PlaylistRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            [
+                'name' => 'required|string',
+                'picture' => 'required|file'
+            ]
+        ];
+    }
+}
