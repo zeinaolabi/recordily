edb77c1709d9f73a1d97e31bd7dd41d5496b7d56
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-06 11:40:34 +0200

    backend: create add playlist api

--- recordily_server/app/Http/Controllers/PlaylistController.php
+++ recordily_server/app/Http/Controllers/PlaylistController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PlaylistRequest;
 use App\Models\Playlist;
 use App\Models\PlaylistHasSong;
 use App\Models\Song;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 class PlaylistController extends Controller
 {
@@ -26,6 +29,32 @@ class PlaylistController extends Controller
         $result = $this->saveSongs($songs);
 
         return response()->json($result);
+    }
+
+    public function addPlaylist(PlaylistRequest $request): JsonResponse
+    {
+        $id = Auth::id();
+
+        $path = public_path() . '/images/' . $id . '/';
+
+        if (!File::exists($path)) {
+            File::makeDirectory($path);
+        }
+
+        try {
+            $picture = $request->file('picture');
+
+            $picture_path = $path . uniqid() . '.' . $picture->extension();
+            file_put_contents($picture_path, $picture->getContent());
+        } catch (Exception $e) {
+            return response()->json(['error' => $e], 400);
+        }
+
+        if(!Playlist::createPlaylist($id, $request->name, $picture_path)){
+            return response()->json('unsuccessfully attempt', 400);
+        }
+
+        return response()->json($picture->extension(), 201);
     }
 
     private function saveSongs($song_ids): array
