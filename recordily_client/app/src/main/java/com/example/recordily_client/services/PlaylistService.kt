8a498d8bb80a89d35bf95277cc58d29b6125762b
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-06 11:37:19 +0200

    frontend: create get playlist songs service

--- recordily_client/app/src/main/java/com/example/recordily_client/services/PlaylistService.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/services/PlaylistService.kt
@@ -2,9 +2,14 @@ package com.example.recordily_client.services
 
 import com.example.recordily_client.apis.RetrofitInstance
 import com.example.recordily_client.responses.PlaylistResponse
+import com.example.recordily_client.responses.SongResponse
 
 class PlaylistService {
     suspend fun getPlaylists(token: String): List<PlaylistResponse>{
         return RetrofitInstance.playlistAPI.getPlaylists(token)
+    }
+
+    suspend fun getPlaylistSongs(token: String, playlist_id: String): List<SongResponse>{
+        return RetrofitInstance.playlistAPI.getPlaylistSongs(token, playlist_id)
     }
 }
