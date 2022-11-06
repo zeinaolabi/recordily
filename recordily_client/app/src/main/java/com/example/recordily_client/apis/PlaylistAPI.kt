eed59f9835893150ff0548a6322635952f808b72
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-06 11:36:16 +0200

    frontend: add get playlist songs api

--- recordily_client/app/src/main/java/com/example/recordily_client/apis/PlaylistAPI.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/apis/PlaylistAPI.kt
@@ -1,10 +1,15 @@
 package com.example.recordily_client.apis
 
 import com.example.recordily_client.responses.PlaylistResponse
+import com.example.recordily_client.responses.SongResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface PlaylistAPI {
     @GET("get_playlists")
     suspend fun getPlaylists(@Header("Authorization") token: String): List<PlaylistResponse>
+
+    @GET("get_playlist_songs/{playlist_id}")
+    suspend fun getPlaylistSongs(@Header("Authorization") token: String, @Path("playlist_id") playlist_id: String): List<SongResponse>
 }
