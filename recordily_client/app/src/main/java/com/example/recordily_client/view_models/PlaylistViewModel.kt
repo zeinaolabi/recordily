352d9ee3ca7066c0fef7a69e5f30017c9de041dc
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-06 17:27:18 +0200

    frontend: call get playlist info api

--- recordily_client/app/src/main/java/com/example/recordily_client/view_models/PlaylistViewModel.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/view_models/PlaylistViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.recordily_client.responses.PlaylistResponse
 import com.example.recordily_client.responses.SongResponse
 import com.example.recordily_client.services.PlaylistService
 import kotlinx.coroutines.launch
@@ -15,9 +16,19 @@ class PlaylistViewModel: ViewModel() {
     val playlistSongsResultLiveData : LiveData<List<SongResponse>>
         get() = playlistSongsResult
 
+    private val playlistResult = MutableLiveData<PlaylistResponse>()
+    val playlistResultLiveData : LiveData<PlaylistResponse>
+        get() = playlistResult
+
     fun getPlaylistSongs(token: String, playlist_id: String){
         viewModelScope.launch {
             playlistSongsResult.postValue(playlistService.getPlaylistSongs(token, playlist_id))
+        }
+    }
+
+    fun getPlaylist(token: String, playlist_id: String){
+        viewModelScope.launch {
+            playlistResult.postValue(playlistService.getPlaylist(token, playlist_id))
         }
     }
 }
