61bc72ac492aaf6311685c76e8100c81c05df74c
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-07 11:04:49 +0200

    backend: create get followed artists api

--- recordily_server/app/Http/Controllers/ArtistController.php
+++ recordily_server/app/Http/Controllers/ArtistController.php
@@ -8,5 +8,16 @@ use Illuminate\Support\Facades\Auth;
 
 class ArtistController extends Controller
 {
+    public function getFollowedArtists(): JsonResponse
+    {
+        $id = Auth::id();
 
+        $followed_artists = Follow::getFollowedArtists($id);
+
+        if ($followed_artists->isEmpty()) {
+            return response()->json('No followed artists', 204);
+        }
+
+        return response()->json($followed_artists);
+    }
 }
