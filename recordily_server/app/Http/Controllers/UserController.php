c27016b0c20886697ade46e46c2a63a78ccdb1f3
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-07 23:00:46 +0200

    backend: create get user info api

--- recordily_server/app/Http/Controllers/UserController.php
+++ recordily_server/app/Http/Controllers/UserController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+   public function getUserInfo(): JsonResponse
+   {
+       $id = Auth::id();
+
+       $user = User::find($id);
+
+       return response()->json($user);
+   }
 }
