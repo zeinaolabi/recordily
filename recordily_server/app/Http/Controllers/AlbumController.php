d9dcfdb20eda70db5585ae2399426a7d15e30749
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-07 12:48:08 +0200

    backend: create get artist albums api

--- recordily_server/app/Http/Controllers/AlbumController.php
+++ recordily_server/app/Http/Controllers/AlbumController.php
@@ -7,5 +7,25 @@ use Illuminate\Http\JsonResponse;
 
 class AlbumController extends Controller
 {
+    public function getArtistAlbums(int $artist_id, int $limit): JsonResponse
+    {
+        $albums = Album::getAlbums($artist_id, $limit);
 
+        return response()->json($albums);
+    }
+
+//    public function createAlbum(): JsonResponse
+//    {
+//        $isCreated = Album::create([
+//            'user_id' => 8,
+//            'picture' => 'test',
+//            'name' => 'test'
+//        ]);
+//
+//        if(!$isCreated){
+//            return response()->json('not created');
+//        }
+//
+//        return response()->json(' created');
+//    }
 }
