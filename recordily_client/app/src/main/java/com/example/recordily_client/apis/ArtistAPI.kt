41d83b17ff7da4f6e4fe7fab4c887903157fccd6
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-07 15:19:24 +0200

    frontend: add get artist info api

--- recordily_client/app/src/main/java/com/example/recordily_client/apis/ArtistAPI.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/apis/ArtistAPI.kt
@@ -4,8 +4,13 @@ import com.example.recordily_client.responses.ArtistResponse
 import com.example.recordily_client.responses.SongResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface ArtistAPI {
     @GET("followed_artists")
     suspend fun followedArtists(@Header("Authorization") token: String): List<ArtistResponse>
+
+    @GET("get_artist_info/{artist_id}")
+    suspend fun getArtist(@Header("Authorization") token: String, @Path("artist_id") artist_id: String): ArtistResponse
+
 }
