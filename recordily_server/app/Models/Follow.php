9f663f032e7a8ec96644dbdbacc274f94ef48b7b
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-07 11:06:28 +0200

    backend: create get followed artists function in follow model

--- recordily_server/app/Models/Follow.php
+++ recordily_server/app/Models/Follow.php
@@ -15,4 +15,19 @@ class Follow extends Model
         'followed_id'
     ];
 
+    public static function getFollowedArtists(int $id): Collection
+    {
+        $artist_type_id = 0;
+
+        return User::whereIn(
+            'id',
+            function ($query) use ($artist_type_id, $id) {
+                $query
+                    ->select('followed_id')
+                    ->from(with(new self())->getTable())
+                    ->where('user_type_id', $artist_type_id)
+                    ->where('follower_id', $id);
+            }
+        )->get();
+    }
 }
