f09c432065abd05ab68afc4c4d22d8baca3176f0
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-07 17:50:17 +0200

    frontend: add get top songs service

--- recordily_client/app/src/main/java/com/example/recordily_client/services/ArtistService.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/services/ArtistService.kt
@@ -3,6 +3,7 @@ package com.example.recordily_client.services
 import com.example.recordily_client.apis.RetrofitInstance
 import com.example.recordily_client.responses.AlbumResponse
 import com.example.recordily_client.responses.ArtistResponse
+import com.example.recordily_client.responses.SongResponse
 
 class ArtistService {
     suspend fun followedArtists(token: String): List<ArtistResponse>{
@@ -31,5 +32,9 @@ class ArtistService {
 
     suspend fun getAlbums(token: String, artist_id: String, limit: Int): List<AlbumResponse>{
         return RetrofitInstance.artistAPI.getAlbums(token, artist_id, limit)
+    }
+
+    suspend fun getArtistTopSongs(token: String, artist_id: String, limit: Int): List<SongResponse>{
+        return RetrofitInstance.artistAPI.getArtistTopSongs(token, artist_id, limit)
     }
 }
