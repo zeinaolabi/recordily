dc164db87e698590e826c873f24b1839aa66d60f
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-07 21:16:05 +0200

    frontend: add get album songs service

--- recordily_client/app/src/main/java/com/example/recordily_client/services/AlbumService.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/services/AlbumService.kt
@@ -2,9 +2,14 @@ package com.example.recordily_client.services
 
 import com.example.recordily_client.apis.RetrofitInstance
 import com.example.recordily_client.responses.AlbumResponse
+import com.example.recordily_client.responses.SongResponse
 
 class AlbumService {
     suspend fun getAlbumInfo(token: String, album_id: String): AlbumResponse{
         return RetrofitInstance.albumAPI.getAlbumInfo(token, album_id)
+    }
+
+    suspend fun getAlbumSongs(token: String, album_id: String): List<SongResponse>{
+        return RetrofitInstance.albumAPI.getAlbumSongs(token, album_id)
     }
 }
