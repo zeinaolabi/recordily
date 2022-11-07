718031fee03c8d3069080863518fe86ac7b86df8
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-07 11:50:09 +0200

    backend: create follow artist api

--- recordily_server/app/Http/Controllers/ArtistController.php
+++ recordily_server/app/Http/Controllers/ArtistController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Follow;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,7 +16,7 @@ class ArtistController extends Controller
         $followed_artists = Follow::getFollowedArtists($id);
 
         if ($followed_artists->isEmpty()) {
-            return response()->json('No followed artists', 204);
+            return response()->json('No followed artists', 400);
         }
 
         return response()->json($followed_artists);
@@ -26,5 +27,24 @@ class ArtistController extends Controller
         $id = Auth::id();
 
         return Follow::checkIfFollowed($id, $artist_id);
+    }
+
+    public function followArtist(int $artist_id): JsonResponse
+    {
+        $id = Auth::id();
+
+        if(!User::isArtist($artist_id))
+        {
+            return response()->json(User::isArtist($artist_id), 400);
+        }
+
+        if (Follow::checkIfFollowed($id, $artist_id))
+        {
+            return response()->json('Already Followed', 400);
+        }
+
+        Follow::followArtist($id, $artist_id);
+
+        return response()->json('Artist Followed', 201);
     }
 }
