5f16cc05af0a441b69a1694bdc015681e26c027c
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-07 21:15:38 +0200

    frontend: add get album songs api

--- recordily_client/app/src/main/java/com/example/recordily_client/apis/AlbumAPI.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/apis/AlbumAPI.kt
@@ -2,6 +2,7 @@ package com.example.recordily_client.apis
 
 import com.example.recordily_client.responses.AlbumResponse
 import com.example.recordily_client.responses.ArtistResponse
+import com.example.recordily_client.responses.SongResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Path
@@ -10,4 +11,6 @@ interface AlbumAPI {
     @GET("get_album_info/{album_id}")
     suspend fun getAlbumInfo(@Header("Authorization") token: String, @Path("album_id") album_id: String): AlbumResponse
 
+    @GET("get_album_songs/{album_id}")
+    suspend fun getAlbumSongs(@Header("Authorization") token: String, @Path("album_id") album_id: String): List<SongResponse>
 }
