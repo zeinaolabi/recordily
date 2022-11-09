3589bb5189aeed73f2fb0eb70903d247d1c3e358
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-09 13:27:59 +0200

    frontend: add get unreleased songs api

--- recordily_client/app/src/main/java/com/example/recordily_client/apis/SongsAPI.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/apis/SongsAPI.kt
@@ -8,25 +8,51 @@ import retrofit2.http.*
 
 interface SongsAPI {
     @GET("auth/suggested_songs/{limit}")
-    suspend fun suggestedSongs(@Header("Authorization") token: String, @Path("limit") limit: Int): List<SongResponse>
+    suspend fun suggestedSongs(
+        @Header("Authorization") token: String,
+        @Path("limit") limit: Int
+    ): List<SongResponse>
 
     @GET("auth/recently_played_songs/{limit}")
-    suspend fun getRecentlyPlayedSongs(@Header("Authorization") token: String, @Path("limit") limit: Int): List<SongResponse>
+    suspend fun getRecentlyPlayedSongs(
+        @Header("Authorization") token: String,
+        @Path("limit") limit: Int
+    ): List<SongResponse>
 
     @GET("auth/top_liked_songs/{limit}")
-    suspend fun getTopLikedSongs(@Header("Authorization") token: String, @Path("limit") limit: Int): List<SongResponse>
+    suspend fun getTopLikedSongs(
+        @Header("Authorization") token: String,
+        @Path("limit") limit: Int
+    ): List<SongResponse>
 
     @GET("auth/top_played_songs/{limit}")
-    suspend fun getTopPlayedSongs(@Header("Authorization") token: String, @Path("limit") limit: Int): List<SongResponse>
+    suspend fun getTopPlayedSongs(
+        @Header("Authorization") token: String,
+        @Path("limit") limit: Int
+    ): List<SongResponse>
 
     @GET("auth/search/{input}")
-    suspend fun searchForSong(@Header("Authorization") token: String, @Path("input") input: String): SearchResponse
+    suspend fun searchForSong(
+        @Header("Authorization") token: String,
+        @Path("input") input: String
+    ): SearchResponse
 
     @GET("auth/liked_songs")
-    suspend fun getLikedSongs(@Header("Authorization") token: String): List<SongResponse>
+    suspend fun getLikedSongs(
+        @Header("Authorization") token: String
+    ): List<SongResponse>
 
     @GET("auth/search_liked_songs/{input}")
-    suspend fun searchLikedSongs(@Header("Authorization") token: String, @Path("input") input: String): List<SongResponse>
+    suspend fun searchLikedSongs(
+        @Header("Authorization") token: String,
+        @Path("input") input: String
+    ): List<SongResponse>
+
+    @GET("auth/unreleased_songs/{limit}")
+    suspend fun getUnreleasedSongs(
+        @Header("Authorization") token: String,
+        @Path("limit") limit: Int
+    ): List<SongResponse>
 
     @Multipart
     @POST("upload_song")
