93ca7d8d48a32bb4ebe8e1a1d660eea0d373f2cc
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-09 12:17:12 +0200

    frontend: call get user top songs api in top songs page

--- recordily_client/app/src/main/java/com/example/recordily_client/pages/common/TopSongsPage.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/pages/common/TopSongsPage.kt
@@ -6,13 +6,23 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.recordily_client.R
 import com.example.recordily_client.components.ExitBar
@@ -22,12 +32,23 @@ import com.example.recordily_client.components.SongCard
 import com.example.recordily_client.navigation.Screen
 import com.example.recordily_client.navigation.navigateTo
 import com.example.recordily_client.responses.SongResponse
+import com.example.recordily_client.view_models.AlbumsViewModel
+import com.example.recordily_client.view_models.LoginViewModel
+import com.example.recordily_client.view_models.TopSongsViewModel
 
 private val popUpVisibility = mutableStateOf(false)
 
 @ExperimentalAnimationApi
 @Composable
 fun TopSongsPage(navController: NavController){
+    val limit = 40
+    val loginViewModel: LoginViewModel = viewModel()
+    val topSongsViewModel: TopSongsViewModel = viewModel()
+    val token = "Bearer " + loginViewModel.sharedPreferences.getString("token", "").toString()
+    topSongsViewModel.getTopSongs(token, limit)
+
+    val songs by topSongsViewModel.topSongsResultLiveData.observeAsState()
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -46,7 +67,7 @@ fun TopSongsPage(navController: NavController){
             ){
                 HorizontalLine()
 
-                TopSongsContent(navController)
+                TopSongsContent(navController, songs)
             }
         }
 
@@ -64,28 +85,48 @@ fun TopSongsPage(navController: NavController){
 }
 
 @Composable
-private fun TopSongsContent(navController: NavController){
+private fun TopSongsContent(navController: NavController, songs: List<SongResponse>?){
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(dimensionResource(id = R.dimen.padding_medium)),
     ){
-        for(i in 1..3){
-            SongCard(
-                song =
-                    SongResponse(1,"",1,"","","",1,
-                    1,"","",1,"")
-                ,
-                onSongClick = {
-                    navigateTo(
-                        navController = navController,
-                        destination = Screen.SongPage.route,
-                        popUpTo = Screen.SuggestedSongsPage.route
-                    )
-                },
-                onMoreClick = { popUpVisibility.value = true }
-            )
-        }
+        if(songs == null || songs.isEmpty()){
+            Column(
+                modifier = Modifier
+                    .width(330.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Icon(
+                    painter = painterResource(id = R.drawable.nothing_found),
+                    contentDescription = "not found",
+                    modifier = Modifier.size(60.dp),
+                    tint = Color.Unspecified
+                )
 
+                Text(
+                    text = "No songs found",
+                    fontSize = dimensionResource(id = R.dimen.font_small).value.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colors.onPrimary
+                )
+            }
+        } else {
+            for (song in songs) {
+                SongCard(
+                    song = song,
+                    onSongClick = {
+                        navigateTo(
+                            navController = navController,
+                            destination = Screen.SongPage.route,
+                            popUpTo = Screen.SuggestedSongsPage.route
+                        )
+                    },
+                    onMoreClick = { popUpVisibility.value = true }
+                )
+            }
+        }
     }
 }
