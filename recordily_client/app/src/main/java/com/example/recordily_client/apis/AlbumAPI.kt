b72cf0e41df4bf8e64a04fe5ea66833c58693502
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-09 19:09:47 +0200

    frontend: add create album api

--- recordily_client/app/src/main/java/com/example/recordily_client/apis/AlbumAPI.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/apis/AlbumAPI.kt
@@ -2,9 +2,8 @@ package com.example.recordily_client.apis
 
 import com.example.recordily_client.responses.AlbumResponse
 import com.example.recordily_client.responses.SongResponse
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Path
+import okhttp3.MultipartBody
+import retrofit2.http.*
 
 interface AlbumAPI {
     @GET("auth/get_album_info/{album_id}")
@@ -24,4 +23,12 @@ interface AlbumAPI {
         @Header("Authorization") token: String,
         @Path("limit") limit: Int
     ): List<AlbumResponse>
+
+    @Multipart
+    @POST("auth/create_album")
+    suspend fun createAlbum(
+        @Header("Authorization") token: String,
+        @Part("name") name: String,
+        @Part file: MultipartBody.Part
+    )
 }
