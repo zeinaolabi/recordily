89727197f622a93281c7225c56bc8cb467b0efe0
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-09 19:10:05 +0200

    frontend: add create album service

--- recordily_client/app/src/main/java/com/example/recordily_client/services/AlbumService.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/services/AlbumService.kt
@@ -3,6 +3,7 @@ package com.example.recordily_client.services
 import com.example.recordily_client.apis.RetrofitInstance
 import com.example.recordily_client.responses.AlbumResponse
 import com.example.recordily_client.responses.SongResponse
+import okhttp3.MultipartBody
 
 class AlbumService {
     suspend fun getAlbumInfo(token: String, album_id: String): AlbumResponse{
@@ -15,5 +16,9 @@ class AlbumService {
 
     suspend fun getUnreleasedAlbum(token: String, limit: Int): List<AlbumResponse>{
         return RetrofitInstance.albumAPI.getUnreleasedAlbums(token, limit)
+    }
+
+    suspend fun addAlbum(token: String, name: String, image: MultipartBody.Part){
+        return RetrofitInstance.albumAPI.createAlbum(token, name, image)
     }
 }
