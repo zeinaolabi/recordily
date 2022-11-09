0f608c16c44082d05b653318b0b872781ad58954
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-09 10:39:57 +0200

    frontend: add user top songs api

--- recordily_client/app/src/main/java/com/example/recordily_client/apis/UserAPI.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/apis/UserAPI.kt
@@ -1,5 +1,6 @@
 package com.example.recordily_client.apis
 
+import com.example.recordily_client.responses.SongResponse
 import com.example.recordily_client.responses.UserResponse
 import okhttp3.MultipartBody
 import retrofit2.http.*
@@ -7,6 +8,9 @@ import retrofit2.http.*
 interface UserAPI {
     @GET("auth/get_user_info")
     suspend fun getUserInfo(@Header("Authorization") token: String): UserResponse
+
+    @GET("auth/get_user_top_songs/{limit}")
+    suspend fun getUserTopSongs(@Header("Authorization") token: String, limit: Int): List<SongResponse>
 
     @Multipart
     @POST("auth/edit_profile")
