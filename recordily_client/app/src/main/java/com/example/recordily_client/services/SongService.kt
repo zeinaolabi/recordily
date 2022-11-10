51c12f9a8c1c2deaa339ab010f501cb27c861801
Zeina Olabi <zeina.olabi02@gmail.com>
2022-11-10 13:50:36 +0200

    frontend: add check if song is liked service

--- recordily_client/app/src/main/java/com/example/recordily_client/services/SongService.kt
+++ recordily_client/app/src/main/java/com/example/recordily_client/services/SongService.kt
@@ -16,35 +16,35 @@ class SongService {
         return RetrofitInstance.songAPI.uploadSong(token, uploadSongRequest, song, image)
     }
 
-    suspend fun getSuggested(token: String, limit: Int): List<SongResponse>{
+    suspend fun getSuggested(token: String, limit: Int): List<SongResponse> {
         return RetrofitInstance.songAPI.suggestedSongs(token, limit)
     }
 
-    suspend fun getTopLiked(token: String, limit: Int): List<SongResponse>{
+    suspend fun getTopLiked(token: String, limit: Int): List<SongResponse> {
         return RetrofitInstance.songAPI.getTopLikedSongs(token, limit)
     }
 
-    suspend fun getRecentlyPlayed(token: String, limit: Int): List<SongResponse>{
+    suspend fun getRecentlyPlayed(token: String, limit: Int): List<SongResponse> {
         return RetrofitInstance.songAPI.getRecentlyPlayedSongs(token, limit)
     }
 
-    suspend fun getTopPlayed(token: String, limit: Int): List<SongResponse>{
+    suspend fun getTopPlayed(token: String, limit: Int): List<SongResponse> {
         return RetrofitInstance.songAPI.getTopPlayedSongs(token, limit)
     }
 
-    suspend fun getSearchResult(token: String, input: String): SearchResponse{
+    suspend fun getSearchResult(token: String, input: String): SearchResponse {
         return RetrofitInstance.songAPI.searchForSong(token, input)
     }
 
-    suspend fun getLikedSongs(token: String): List<SongResponse>{
+    suspend fun getLikedSongs(token: String): List<SongResponse> {
         return RetrofitInstance.songAPI.getLikedSongs(token)
     }
 
-    suspend fun searchLikedSongs(token: String, input: String): List<SongResponse>{
+    suspend fun searchLikedSongs(token: String, input: String): List<SongResponse> {
         return RetrofitInstance.songAPI.searchLikedSongs(token, input)
     }
 
-    suspend fun getUnreleasedSong(token: String, limit: Int): List<SongResponse>{
+    suspend fun getUnreleasedSong(token: String, limit: Int): List<SongResponse> {
         return RetrofitInstance.songAPI.getUnreleasedSongs(token, limit)
     }
 
@@ -54,5 +54,9 @@ class SongService {
 
     suspend fun deleteFromAlbum(token: String, song_id: Int) {
         return RetrofitInstance.songAPI.deleteFromAlbum(token, song_id)
+    }
+
+    suspend fun isLiked(token: String, song_id: Int): Boolean {
+        return RetrofitInstance.songAPI.isLiked(token, song_id)
     }
 }
